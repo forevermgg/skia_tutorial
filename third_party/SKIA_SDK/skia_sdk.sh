8950a37e4830757279c6dd9aa5805b7c46d69220
centforever <centforever@163.com>
2024-03-09 17:16:27 +0800

    skia-pack

--- third_party/SKIA_SDK/skia_sdk.sh
+++ third_party/SKIA_SDK/skia_sdk.sh
@@ -1,3 +1,4 @@
+# https://github.com/JetBrains/skia-pack/releases/
 wget https://github.com/JetBrains/skia-pack/releases/download/m116-47d3027-1/Skia-m116-47d3027-1-macos-Debug-arm64.zip
 wget https://github.com/JetBrains/skia-pack/releases/download/m116-47d3027-1/Skia-m116-47d3027-1-macos-Debug-x64.zip
 for file in ./*.zip; do
