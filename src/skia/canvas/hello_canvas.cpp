c04a2d43d8917252c0901bcbeba835a3a4255b25
centforever <centforever@163.com>
2024-03-09 23:21:11 +0800

    test

--- src/skia/canvas/hello_canvas.cpp
+++ src/skia/canvas/hello_canvas.cpp
@@ -1,10 +1,13 @@
 #include <include/core/SkCanvas.h>
 #include <include/core/SkBitmap.h>
 #include <include/core/SkPaint.h>
+#include <include/core/SkPath.h>
 #include <include/core/SkTypeface.h>
 #include <include/core/SkFont.h>
 #include <include/codec/SkCodec.h>
 #include <include/core/SkStream.h>
+#include <include/core/SkTextBlob.h>
+#include <include/core/SkRRect.h>
 #include <include/encode/SkPngEncoder.h>
 #include "../file.h"
 
@@ -24,8 +27,37 @@ void draw(SkCanvas *canvas) {
 
     // Translate and draw the text:
     canvas->save();
-    canvas->translate(SkIntToScalar(50), SkIntToScalar(100));
+    canvas->translate(SkIntToScalar(300), SkIntToScalar(300));
     canvas->drawSimpleText(message, strlen(message), SkTextEncoding::kUTF8, 0, 0, font, paint);
+    canvas->restore();
+}
+
+void draw1(SkCanvas* canvas) {
+    const SkScalar scale = 256.0f;
+    const SkScalar R = 0.45f * scale;
+    const SkScalar TAU = 6.2831853f;
+    SkPath path;
+    path.moveTo(R, 0.0f);
+    for (int i = 1; i < 7; ++i) {
+        SkScalar theta = 3 * i * TAU / 7;
+        path.lineTo(R * cos(theta), R * sin(theta));
+    }
+    path.close();
+    SkPaint p;
+    p.setAntiAlias(true);
+    // canvas->clear(SK_ColorWHITE);
+    canvas->translate(0.5f * scale, 0.5f * scale);
+    canvas->drawPath(path, p);
+}
+
+void draw2(SkCanvas* canvas) {
+    canvas->save();
+    canvas->translate(SkIntToScalar(128), SkIntToScalar(128));
+    canvas->rotate(SkIntToScalar(45));
+    SkRect rect = SkRect::MakeXYWH(-90.5f, -90.5f, 181.0f, 181.0f);
+    SkPaint paint;
+    paint.setColor(SK_ColorBLUE);
+    canvas->drawRect(rect, paint);
     canvas->restore();
 }
 
@@ -43,6 +75,8 @@ int main(int argc, char *const argv[]) {
     SkCanvas canvas(bitmap);
 
     draw(&canvas);
+    draw1(&canvas);
+    draw2(&canvas);
 
     // 保存到文件
     SkFILEWStream file((current_directory + PATH_SEPARATOR + "hello_canvas.png").c_str());  //创建文件输出流
