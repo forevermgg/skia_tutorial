2531eab2b4ec4d44c4d46e912c47e76ed14e3c3c
centforever <centforever@163.com>
2024-03-09 17:15:21 +0800

    test

--- third_party/SKIA_SDK/skia_sdk.sh
+++ third_party/SKIA_SDK/skia_sdk.sh
@@ -1,3 +1,7 @@
-wget https://github.com/JetBrains/skia-pack/releases/download/m116-47d3027-1/Skia-m116-47d3027-1-macos-Debug-arm64.zip
-wget https://github.com/JetBrains/skia-pack/releases/download/m116-47d3027-1/Skia-m116-47d3027-1-macos-Debug-x64.zip
-unzip
+# wget https://github.com/JetBrains/skia-pack/releases/download/m116-47d3027-1/Skia-m116-47d3027-1-macos-Debug-arm64.zip
+# wget https://github.com/JetBrains/skia-pack/releases/download/m116-47d3027-1/Skia-m116-47d3027-1-macos-Debug-x64.zip
+for file in ./*.zip; do
+    mkdir ${file%????}
+    tar -xvf $file -C ./${file%????}
+    rm $file
+done
