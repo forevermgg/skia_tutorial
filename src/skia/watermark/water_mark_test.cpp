5723b6d528435341af874f7972e3870784aae4ce
centforever <centforever@163.com>
2024-03-11 20:21:35 +0800

    test

--- src/skia/watermark/water_mark_test.cpp
+++ src/skia/watermark/water_mark_test.cpp
@@ -13,31 +13,52 @@
 #include "include/core/SkImage.h"
 #include "include/core/SkTileMode.h"
 #include "include/core/SkShader.h"
-#include "../file.h"
+#include "include/codec/SkPngDecoder.h"
+#include <iostream>
+#include <string>
+#include <filesystem>
 
-static void write_png(const char *path, sk_sp<SkImage> img) {
+bool loadImageToBitmap(std::string imagePath, SkBitmap* bitmap) {
+    sk_sp<SkData> data = SkData::MakeFromFileName(imagePath.c_str());
+    if (!data) {
+        std::cerr << "Failed to create SkData from file: " << imagePath << std::endl;
+        return false;
+    }
+    std::unique_ptr<SkCodec>  test_codec = SkCodec::MakeFromData(data);
+    // std::unique_ptr<SkCodec>  test_codec(SkCodec::MakeFromData(data));
+    if (!test_codec) {
+        std::cerr << "Failed to create SkCodec from SkData." << std::endl;
+        return false;
+    }
+    SkCodec::Result result;
+    std::unique_ptr<SkCodec> codec = SkPngDecoder::Decode(SkMemoryStream::Make(data), &result);
+    if (result != SkCodec::Result::kSuccess) {
+        std::string errorMsg = SkStringPrintf("Could not create codec for \"%s\": %s.",
+                                              imagePath.c_str(),
+                                           SkCodec::ResultToString(result))
+                .c_str();
+        std::cerr << "Failed to decode image: " << imagePath << std::endl;
+        return false;
+    }
+
+    // 设置SkBitmap的信息
+    SkImageInfo info = codec->getInfo().makeColorType(kN32_SkColorType);
+    bitmap->allocPixels(info);
+
+    // 解码图片数据到SkBitmap
+    if (codec->getPixels(info, bitmap->getPixels(), bitmap->rowBytes()) != SkCodec::kSuccess) {
+        std::cerr << "Failed to decode image: " << imagePath << std::endl;
+        return false;
+    }
+
+    return true;
+}
+
+/*static void write_png(const char *path, sk_sp<SkImage> img) {
     sk_sp<SkData> png = SkPngEncoder::Encode(nullptr, img.get(), {});
     SkASSERT(png);
     SkFILEWStream(path).write(png->data(), png->size());
-}
-
-sk_sp<SkImage> make_raster_image(const char *path) {
-    SkFILEStream stream(path);
-    if (!stream.isValid()) {
-        return nullptr;
-    }
-
-    auto data = SkData::MakeFromFileName(path);
-    if (!data) {
-        return nullptr;
-    }
-    sk_sp<SkImage> image = SkImages::DeferredFromEncodedData(data);
-    if (!image) {
-        SkDebugf("Couldn't decode %s as an SkImage or an ICC profile.\n", path);
-        return nullptr;
-    }
-    return nullptr;
-}
+}*/
 
 void draw(SkCanvas *canvas, sk_sp<SkImage> &image) {
     canvas->clear(SK_ColorWHITE);
@@ -50,29 +71,44 @@ void draw(SkCanvas *canvas, sk_sp<SkImage> &image) {
     canvas->drawPaint(paint);
 }
 
+void DrawBackgroundBitmap(const SkBitmap& bitmap, SkCanvas *canvas, const SkRect* prcDst) {
+    SkSamplingOptions sampling;
+    sk_sp<SkShader> bg_shader = bitmap.makeShader(SkTileMode::kRepeat,
+                                                                SkTileMode::kRepeat, sampling, nullptr);
+    SkPaint bg_paint;
+    bg_paint.setShader(bg_shader);
+    canvas->drawRect(*prcDst, bg_paint);
+}
+
+void DrawBackgroundBitmap(const SkBitmap& bitmap, SkCanvas *canvas, float l, float t, float r, float b) {
+    SkRect rect = SkRect::MakeLTRB(l, t, r, b);
+    DrawBackgroundBitmap(bitmap, canvas, &rect);
+}
+
 int main() {
-    // 创建一个位图设备
+    //加载图片资源
     SkBitmap bitmap;
-    SkImageInfo imageInfo = SkImageInfo::Make(400, 400, kBGRA_8888_SkColorType, kPremul_SkAlphaType); //设置位图信息
-    bitmap.allocPixels(imageInfo, imageInfo.minRowBytes()); //为位图设备绑定信息和分配内存
-    // 创建画布
-    SkCanvas canvas(bitmap);
-
-    sk_sp<SkImage> image = make_raster_image(
-        "/Users/centforever/CLionProjects/skia_tutorial/src/skia/watermark/watermark_test.png");
-    if (image) {
-        draw(&canvas, image);
+    std::string file = "resources/watermark_test.png";
+    if (std::filesystem::exists(file)) {
+        std::cerr << "File found: " << file << std::endl;
+    } else {
+        std::cerr << "File not found: " << file << std::endl;
     }
-
-    //将绘制结果保存到图片文件中
-    std::string current_directory = File::get_current_directory();
-    std::filesystem::path current_path = std::filesystem::current_path();
-    current_directory = current_path.parent_path().parent_path();
-    std::cout << "当前目录是: " << current_directory << std::endl;
-    SkFILEWStream file((current_directory + PATH_SEPARATOR + "watermark_test_result.png").c_str()); //创建文件输出流
-    if (!file.isValid()) {
-        return 1;
+    SkString path(file.c_str());
+    sk_sp<SkData> data = SkData::MakeFromFileName(path.c_str());
+    if (!data) {
+        printf("Missing file %s", path.c_str());
+        return 0;
     }
-    SkPngEncoder::Encode(&file, bitmap.pixmap(), {});
+    std::cout << "Successfully read " << data->size() << " bytes from file: " << file << std::endl;
+    auto codec = SkCodec::MakeFromData(std::move(data));
+    SkImageInfo info = codec->getInfo();
+    bitmap.allocPixels(SkImageInfo::Make(info.width(), info.height(), kN32_SkColorType, kPremul_SkAlphaType));
+    SkCodec::Result result = codec->getPixels(info, bitmap.getPixels(), bitmap.rowBytes());
+    if (result == SkCodec::kSuccess) {
+        printf("#Codec success\n");
+    } else {
+        printf("#Codec failure\n");
+    }
     return 0;
 }
