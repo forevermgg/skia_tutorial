e7b1cbe0be2c2b1c8f738da9e6d0a10fcaa150d7
centforever <centforever@163.com>
2024-03-12 15:57:37 +0800

    test

--- src/skia/test/test_skdata.cpp
+++ src/skia/test/test_skdata.cpp
@@ -4,16 +4,55 @@
 #include <include/core/SkCanvas.h>
 #include <include/core/SkBitmap.h>
 #include <include/core/SkPaint.h>
-#include <include/core/SkTypeface.h>
+#include <include/core/SkPath.h>
+#include <include/core/SkSurface.h>
 #include <include/core/SkFont.h>
 #include <include/codec/SkCodec.h>
 #include <include/core/SkStream.h>
+#include <include/core/SkTextBlob.h>
 #include <include/encode/SkPngEncoder.h>
-#include "../file.h"
+#include "include/gpu/GrDirectContext.h"
+#include "include/core/SkData.h"
+#include "include/core/SkImage.h"
+#include "include/core/SkTileMode.h"
+#include "include/core/SkShader.h"
+#include "include/codec/SkPngDecoder.h"
+#include "skia/file.h"
+#include <iostream>
+#include <string>
+#include <filesystem>
+
+SkBitmap make_background() {
+    SkBitmap background;
+    SkBitmap bitmap;
+    bitmap.allocN32Pixels(32, 32);
+    bitmap.eraseColor(SK_ColorWHITE);
+    SkCanvas tmp(bitmap);
+    SkPaint gray;
+    gray.setColor(SkColorSetARGB(0xFF, 0xEE, 0xEE, 0xEE));
+    tmp.drawRect({0,0,16,16}, gray);
+    tmp.drawRect({16,16,32,32}, gray);
+    SkPaint shader;
+   /* shader.setShader(
+            bitmap.makeShader(SkShader::kRepeat_TileMode, SkShader::kRepeat_TileMode));*/
+    shader.setShader(bitmap.makeShader(SkTileMode::kRepeat, SkTileMode::kRepeat, SkSamplingOptions()));
+    background.allocN32Pixels(612, 792);
+    SkCanvas tmp2(background);
+    tmp2.drawPaint(shader);
+    return background;
+}
 
 int main(int argc, char *const argv[]) {
     std::filesystem::path current_path = std::filesystem::current_path();
     std::string current_directory = current_path.parent_path().parent_path();
     std::cout << "当前目录是: " << current_directory << std::endl;
-    return 1;
+
+    SkBitmap bitmap = make_background();
+
+    // 保存到文件
+    SkFILEWStream file((current_directory + PATH_SEPARATOR + "test_sk_data.png").c_str());  //创建文件输出流
+    if (!file.isValid()) {
+        return 1;
+    }
+    return SkPngEncoder::Encode(&file, bitmap.pixmap(), {});
 }
--- third_party/SKIA_SDK/skia_sdk.sh
+++ third_party/SKIA_SDK/skia_sdk.sh
@@ -1,6 +1,6 @@
 # https://github.com/JetBrains/skia-pack/releases/
-wget https://github.com/JetBrains/skia-pack/releases/download/m116-47d3027-1/Skia-m116-47d3027-1-macos-Debug-arm64.zip
-wget https://github.com/JetBrains/skia-pack/releases/download/m116-47d3027-1/Skia-m116-47d3027-1-macos-Debug-x64.zip
+wget https://github.com/JetBrains/skia-pack/releases/download/m116-47d3027-1/Skia-m116-47d3027-1-macos-Release-arm64.zip
+wget https://github.com/JetBrains/skia-pack/releases/download/m116-47d3027-1/Skia-m116-47d3027-1-macos-Release-x64.zip
 for file in ./*.zip; do
     mkdir ${file%????}
     tar -xvf $file -C ./${file%????}
