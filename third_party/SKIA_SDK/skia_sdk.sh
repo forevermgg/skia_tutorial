0d337f6f7430f30dc12f9463ef0103bed4237ce2
centforever <centforever@CentForeverdeMacBook-Pro.local>
2024-03-23 14:10:05 +0800

    first commit

--- third_party/SKIA_SDK/skia_sdk.sh
+++ third_party/SKIA_SDK/skia_sdk.sh
@@ -1,4 +1,5 @@
 # https://github.com/JetBrains/skia-pack/releases/
+rm -rf Skia-*/
 wget https://github.com/JetBrains/skia-pack/releases/download/m116-47d3027-1/Skia-m116-47d3027-1-macos-Release-arm64.zip
 wget https://github.com/JetBrains/skia-pack/releases/download/m116-47d3027-1/Skia-m116-47d3027-1-macos-Release-x64.zip
 for file in ./*.zip; do
